75292937360fee5686ca01585b553e3ef2679c3f
Pedro Oliveira <designer.pedroliveira@gmail.com>
2024-05-06 08:45:54 -0300

    Feat: Added modal feature and save user responses

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -27,11 +27,45 @@ var slide = new Swiper(".depositionsCarousel", {
 });
 
 const modalForm = document.getElementById("modalOverlay");
+const form = document.getElementById("contactForm");
 
-modalForm.addEventListener("click", function (event) {
+modalForm.addEventListener("click", (event) => {
   event.target === modalForm ? modalForm.classList.remove("open") : null;
 });
 
 function openModal() {
   modalForm.classList.add("open");
 }
+
+function closeModal() {
+  modalForm.classList.remove("open");
+}
+
+function cleanForm() {
+  var name = (document.getElementById("nameInput").value = "");
+  var lastName = (document.getElementById("lastNameInput").value = "");
+  var email = (document.getElementById("emailInput").value = "");
+  var phone = (document.getElementById("phoneInput").value = "");
+  var mensage = (document.getElementById("mensageInput").value = "");
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  var name = document.getElementById("nameInput").value;
+  var lastName = document.getElementById("lastNameInput").value;
+  var email = document.getElementById("emailInput").value;
+  var phone = document.getElementById("phoneInput").value;
+  var mensage = document.getElementById("mensageInput").value;
+
+  const user = {
+    name: name,
+    lastName: lastName,
+    email: email,
+    phone: phone,
+    mensage: mensage,
+  };
+
+  cleanForm();
+  closeModal();
+});
